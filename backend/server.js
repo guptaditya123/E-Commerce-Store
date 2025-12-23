7c2337907bae5f670fd9070cf43e346facab3d62
Aditya kumar <guptaditya81@gmail.com>
2025-12-23 23:56:49 +0530

    feat:import productRoutes

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,22 @@
-import express from 'express';
-import dotenv from 'dotenv'
-import authRoutes from './routes/auth.route.js'
-import { connectDB } from './lib/db.js';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
-dotenv.config()
-const app=express();
-const PORT = process.env.PORT || 5000
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.use(express.json()) // use to parse user sending data into json
-app.use(cookieParser()) 
-// Routing 
-app.use('/api/auth',authRoutes)
+app.use(express.json()); // use to parse user sending data into json
+app.use(cookieParser());
 
+// Routing
+app.use("/api/auth", authRoutes);
+app.use("/api/product", productRoutes);
 
-app.listen(PORT,()=>{
-    console.log(`server is started at port ${PORT}`);
-    connectDB()
-})
+app.listen(PORT, () => {
+  console.log(`server is started at port ${PORT}`);
+  connectDB();
+});
