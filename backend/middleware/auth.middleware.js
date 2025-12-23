daa3aa8b5e39762858fd85d4288103cda75be83a
Aditya kumar <guptaditya81@gmail.com>
2025-12-23 23:54:58 +0530

    feat:Add protectRoute logic

---
+++ backend/middleware/auth.middleware.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken"
+import User from '../models/user.model.js'
+export const protectRoute = async(req,res)=>{
+    try {
+        const accessToken = req.cookies.accessToken;
+        if(!accessToken){
+            return res.status(401).json({message:"Unauthorized - Access Token not found"})
+        }
+
+        const decoded = jwt.verify(accessToken,process.env.ACCESS_TOKEN_KEY);
+        const user = await User.findById(decoded.id).select("-password");
+
+        if(!user){
+            return res.status(401).json({message:"User not found"});
+        }
+
+        req.user = user;
+        next();
+
+    } catch (error) {
+        console.log("Error in protectedRoute middleware",error.message);
+        return res.status(401).json({message:"Unauthorized - Invalid access Token"})
+    }
+}
