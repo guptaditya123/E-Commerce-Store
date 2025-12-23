795d31b0e97c8af9ad30d516f02e4782e0e3460c
Aditya kumar <guptaditya81@gmail.com>
2025-12-23 23:20:33 +0530

    feat: add login and refresh token API endpoints

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { signup, login, logout } from "../controllers/auth.controller.js";
+import { signup, login, logout , refreshToken} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
@@ -8,5 +8,7 @@ router.post("/signup", signup);
 router.post("/login", login);
 
 router.post("/logout", logout);
+
+router.post("/refresh-token", refreshToken)
 
 export default router;
