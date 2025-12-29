3bd55e062e6e3c5fce3c188e0d50bb50c1700438
Aditya kumar <guptaditya81@gmail.com>
2025-12-29 23:37:16 +0530

    feat:add controller for fetching products, deleting products, creating products

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,3 +1,5 @@
+import redis from "../lib/redis.js";
+import cloudinary from '../lib/cloudinary.js'
 import Product from "../models/product.model.js";
 
 export const getAllProducts = async()=>{
@@ -7,5 +9,74 @@ export const getAllProducts = async()=>{
     }catch(err){
         console.log(err)
         res.status(500).json("Internal server error")
+    }
+}
+
+export const getFeaturedProducts= async(req,res)=>{
+    try {
+        let featuredProducts = await redis.get("featured_products");
+        if(featuredProducts){
+            return res.json(JSON.parse(featuredProducts));
+        }
+
+        // if not in redis ,the fetch from mongodb
+        // .lean()- will return plain js objects instead of mongodb documents
+        // which is good for performance
+        featuredProducts = await Product.find({isFeatured:true}).lean()
+
+        if(!featuredProducts){
+            return res.status(404).json({message:"No Featured Products found"})
+        }
+        await redis.set("featured_products",JSON.stringify(featuredProducts));
+        res.status(200).json(featuredProducts);
+    } catch (error) {
+     console.log("Error in fetching featured product", error);
+     res.status(500).json({message:"Server error:,",error:error.message})   
+    }
+}
+
+
+export const createProduct = async(req,res)=>{
+    try {
+        const {name,description,price,image,category} = req.body;
+        let cloudinaryResponse = null;
+        if(image){
+            cloudinaryResponse = await cloudinary.uploader.upload(image,{folder:"products"})
+        }
+        const product = await Product.create({
+            name,
+            description,
+            price,
+            image:cloudinaryResponse?.secure_url? cloudinaryResponse.secure_url:"",
+            category
+        })
+        res.status(201).json(product);
+    } catch (error) {
+        
+    }
+}
+
+
+export const deleteProduct = async (req,res)=>{
+    try {
+        const product = await Product.findById(req.params.id);
+        if(!product){
+            return res.status(404).json({message:"Product not found"})
+        }
+        if(product.image){
+            const publicId = product.image.split("/").pop().split(".")[0];
+            try {
+                await cloudinary.uploader.destroy(`products/${publicId}`);
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        await product.findByIdAndDelete(req.params.id);
+        res.status(200).json({message:"Product deleted successfully"})
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message:"Server error",error:error.message})
     }
 }
