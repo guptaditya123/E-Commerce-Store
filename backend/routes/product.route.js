e5d9cfecc798901bf95d189603007347f186bbb1
Aditya kumar <guptaditya81@gmail.com>
2025-12-29 23:40:35 +0530

    feat:add product related routes

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,9 +1,14 @@
 import express from "express"
-import { getAllProducts } from "../controllers/product.controller.js";
+import { getAllProducts, getFeaturedProducts } from "../controllers/product.controller.js";
+import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
 
 const productRouter = express.Router()
 
-productRouter.get('/',getAllProducts)
+productRouter.get('/',protectRoute,adminRoute,getAllProducts);
+productRouter.get('/featuredProducts',getFeaturedProducts);
+productRouter.post('/createProduct',protectRoute,adminRoute,createProduct);
+productRouter.post('/deleteProduct/:id',protectRoute,adminRoute,deleteProduct);
+
 
 
 export default productRouter;
