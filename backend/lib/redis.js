132d12023828535fee93f20dc9e803e36dfb64f6
Aditya kumar <guptaditya81@gmail.com>
2025-12-29 23:41:07 +0530

    feat:fix bug

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -2,4 +2,5 @@ import Redis from "ioredis"
 import dotenv from "dotenv"
 
 dotenv.config();
-export const redis = new Redis(process.env.UPSTASH_REDIS);
+const  redis = new Redis(process.env.UPSTASH_REDIS);
+export default redis;
