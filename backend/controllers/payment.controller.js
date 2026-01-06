065858358022352648fb160e85744a79d84cb9fe
Aditya kumar <guptaditya81@gmail.com>
2026-01-06 10:03:48 +0530

    add logic for payment  checkout session

---
+++ backend/controllers/payment.controller.js
@@ -0,0 +1,93 @@
+
+import stripe from "../lib/stripe.js";
+import coupon from "../models/coupon.model.js";
+
+
+export const createCheckoutSession =  async (req, res) => {
+    try {
+      // products and coupon can be received
+      const { products, couponCode } = req.body;
+      if (!Array.isArray(products) || products.length === 0) {
+        return res
+          .status(400)
+          .json({ error: "Invalid or empty products  array" });
+      }
+      let totalAmount = 0;
+      let lineItems = products.map((product) => {
+        const amount = Math.round(product.price);
+        totalAmount += amount * product.quantity;
+
+        return {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: product.name,
+              image: [product.image],
+            },
+            unit_amount: amount,
+          },
+        };
+      });
+
+      let coupon = null;
+      if (couponCode) {
+        coupon = await coupon.findOne({
+          code: couponCode,
+          userId: req.user,
+          isActive: true,
+        });
+
+        if (coupon) {
+          totalAmount -= Math.round(
+            (totalAmount * coupon.discountPercentage) / 100
+          );
+        }
+      }
+
+      const session = stripe.checkout.sessions.create({
+        payment_method_routes: ["card"],
+        line_items: lineItems,
+        mode: "payment",
+        success_url: `${process.env.CLIENT_URL}/purchase-success?session_id = {CHECKOUT_SESSION_ID}`,
+        cancel_url: `${process.env.CLIENT_URL}/purchase-cancel`,
+        discounts: coupon
+          ? [
+              {
+                coupon: await createStripeCoupon(coupon.discountPercentage),
+              },
+            ]
+          : [],
+          metadata:{
+            userId:req.user._id.toString(),
+            couponCode:couponCode || ""
+          }
+      });
+      if(totalAmount >= 2000){
+        await createNewCoupon(req.user._id.toString());
+      }
+      res.status(200).json({id:session.id,totalAmount:totalAmount/100});
+    } catch (error) {
+      console.log("Error in createCheckoutSession controller", error.message);
+      res.status(500).json({ message: "Server error", error: error.message });
+    }
+  };
+
+
+async function createStripeCoupon(discountPercentage) {
+  const coupon = await stripe.coupons.create({
+    percent_off: discountPercentage,
+    duration: "once",
+  });
+  return coupon.id;
+}
+
+
+async function createNewCoupon(userId){
+    const newCoupon = new coupon({
+        code:"GIFT" + Math.random().toString(36).substring(2,8).toUpperCase(),
+        discountPercentage:10,
+        expirationDate:new Date(Date.now()+36*24*60*60*1000),
+        userId:userId
+    })
+    await newCoupon.save()
+}
