84dda67a3a9b8931ff737d747040fe4587ab6325
Aditya kumar <guptaditya81@gmail.com>
2026-01-08 09:49:33 +0530

    feat: add analytics routes and fix import paths in server

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,12 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
-import paymentRoutes from '../routes/payment.route.js'
+import analyticsRoutes from "./routes/analytics.route.js"
+import paymentRoutes from './routes/payment.route.js'
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import couponRoutes from "./routes/coupon.route.js";
+
 
 dotenv.config();
 const app = express();
@@ -22,6 +24,7 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/coupon", couponRoutes);
 app.use("/api/coupon", couponRoutes);
 app.use("/api/payment", paymentRoutes);
+app.use("/api/analytics",analyticsRoutes)
 
 app.listen(PORT, () => {
   console.log(`server is started at port ${PORT}`);
