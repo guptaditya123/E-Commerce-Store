5c1c075d71febc6f270b7c09ba5ffbac5a3298af
Aditya kumar <guptaditya81@gmail.com>
2026-01-11 23:25:52 +0530

    feat(app): add Toaster and gradient tweaks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import SignUp from "./pages/SignUp"
 import Navbar from "./components/Navbar"
 import HomePage from "./pages/HomePage"
 import Login from "./pages/Login"
+import { Toaster } from "react-hot-toast"
 
 
 
@@ -10,8 +11,9 @@ function App() {
 
   return (
     <div className='min-h-screen bg-gray-900 text-white relative overflow-hidden'>
-			{/* Background gradient */}
-			<div className='absolute inset-0 overflow-hidden'>
+      {/* Background gradient */}
+      
+      <div className='absolute inset-0 overflow-hidden'>
 				<div className='absolute inset-0'>
 					<div className='absolute top-0 left-1/2 -translate-x-1/2 w-full h-full bg-[radial-gradient(ellipse_at_top,rgba(16,185,129,0.3)_0%,rgba(10,80,60,0.2)_45%,rgba(0,0,0,0.1)_100%)]' />
 				</div>
@@ -24,6 +26,7 @@ function App() {
         <Route  path="/login" element={<Login />} />
       </Routes>
       </div>
+      <Toaster />
     </div>
   )
 }
