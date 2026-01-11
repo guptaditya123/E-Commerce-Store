68e60d970e6d3ff74feaffb7cf5cb45e18088f7a
Aditya kumar <guptaditya81@gmail.com>
2026-01-11 23:25:51 +0530

    feat(store): add user store with signup

---
+++ frontend/src/store/userStore.js
@@ -0,0 +1,26 @@
+import { create } from "zustand";
+import {toast} from "react-hot-toast";
+import axios from "../lib/axios";
+
+
+export const userStore=create((set,get)=>({
+    user:null,
+    loading:false,
+    checkingAuth:true,
+
+    signup: async({name,email,password,confirmPassword})=>{
+        set({loading:true});
+        if(password!=confirmPassword){
+            set({loading:false});
+            return toast.error("Passwords do not match")
+        }
+        try {
+            const res = await axios.post('/auth/signup',{name,email,password});
+            set({user:res.data,loading:false});
+        } catch (error) {
+            set({loading:false});
+            toast.error(error.response.data.message || "An error occurred")
+        }
+    }
+
+}))
