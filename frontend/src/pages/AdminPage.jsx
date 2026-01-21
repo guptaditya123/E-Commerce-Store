3807df5f18d222938d078c04fb220fb8563d5b87
Aditya kumar <guptaditya81@gmail.com>
2026-01-21 09:43:32 +0530

    Update AdminPage component

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import CreateProductForm from "../components/CreateProductForm";
 import ProductList from "../components/ProductList";
 import AnalyticsTab from "../components/AnalyticsTab";
+import { motion } from "framer-motion";
 
 const tabs = [
   { id: "create", label: "Create Product", icon: PlusCircle },
@@ -15,27 +16,29 @@ const AdminPage = () => {
     <div>
       <div>
         <motion.h1
-          className="text-4xl font-bold mb-8 text-emerald-400 text-center"
+          className="text-4xl font-bold mb-4 mt-6 text-emerald-400 text-center"
           initial={{ opacity: 0, y: -20 }}
-          animate={{ opacity: 1, u: 0 }}
+          animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8 }}
         >
           Admin Dashboard
         </motion.h1>
 
-        <div>
-            {tabs.map((tab)=>{
-                <button
-                key={tab.id}
-                onClick={()=>setActiveTab(tab.id)}
-                className={`flex items-center px-4 mx-2 rounded-md transition-colors duration-200${
-                activeTab ===tab.id? "bg-emerald-600":"bg-gray-700 hover:bg-gray-400"
-                }`}
-                >
-                    <tab.icon className="mr-2 h-5 w-5" />
-                    {tab.label}
-                </button>
-            })}
+        <div className="flex justify-center mb-8">
+          {tabs.map((tab) => (
+            <button
+              key={tab.id}
+              onClick={() => setActiveTab(tab.id)}
+              className={`flex items-center px-4 py-2 mx-2 rounded-md transition-colors duration-200 ${
+                activeTab === tab.id
+                  ? "bg-emerald-600 text-white"
+                  : "bg-gray-700 text-gray-300 hover:bg-gray-600"
+              }`}
+            >
+              <tab.icon className="mr-2 h-5 w-5" />
+              {tab.label}
+            </button>
+          ))}
         </div>
         {activeTab === "create" && <CreateProductForm />}
         {activeTab === "products" && <ProductList />}
