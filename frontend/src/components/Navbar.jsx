805dd9c02a70668a18e92bfe12f81c87d5f0c757
Aditya kumar <guptaditya81@gmail.com>
2026-01-22 10:11:46 +0530

    fix: enable admin dashboard link and format code

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 import { userStore } from "../store/userStore";
 
 const Navbar = () => {
-  const {user,logout} = userStore(); // Replace with actual authentication logic
-  // const isAdmin=user.role === 'admin'
+  const { user, logout } = userStore(); // Replace with actual authentication logic
+  const isAdmin = user?.role === "admin";
   return (
     <header className="fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800">
       <div className="container mx-auto px-4 py-3">
@@ -42,7 +42,7 @@ const Navbar = () => {
               </Link>
             )}
 
-            {/* {isAdmin && (
+            {isAdmin && (
               <Link
                 className="bg-emerald-700 hover:bg-emerald-600 text-white px-3 py-1 rounded-md font-medium
 								 transition duration-300 ease-in-out flex items-center"
@@ -51,32 +51,37 @@ const Navbar = () => {
                 <Lock className="inline-block mr-1 " size={18} />
                 <span className="hidden sm:inline">Dashboard</span>
               </Link>
-            )} */}
+            )}
 
             {user ? (
               <button
-              	className='bg-gray-700 hover:bg-gray-600 text-white py-2 px-4 
-						rounded-md flex items-center transition duration-300 ease-in-out'
-            onClick={logout}
-							>
-                	<LogOut size={18} />
-								<span className='hidden sm:inline ml-2'>Log Out</span>
-							</button>
+                className="bg-gray-700 hover:bg-gray-600 text-white py-2 px-4 
+						rounded-md flex items-center transition duration-300 ease-in-out"
+                onClick={logout}
+              >
+                <LogOut size={18} />
+                <span className="hidden sm:inline ml-2">Log Out</span>
+              </button>
             ) : (
               <>
-                <Link to="/signup"
-                className="	className='bg-emerald-600 hover:bg-emerald-700 text-white py-2 px-4 
+                <Link
+                  to="/signup"
+                  className="	className='bg-emerald-600 hover:bg-emerald-700 text-white py-2 px-4 
 									rounded-md flex items-center transition duration-300 ease-in-out'
 								"
                 >
-                  <UserPlus className='mr-2' size={18} />
-                  Sign up</Link>
-                <Link to="/login"
-                className="className='bg-gray-700 hover:bg-gray-600 text-white py-2 px-4 
+                  <UserPlus className="mr-2" size={18} />
+                  Sign up
+                </Link>
+                <Link
+                  to="/login"
+                  className="className='bg-gray-700 hover:bg-gray-600 text-white py-2 px-4 
 									rounded-md flex items-center transition duration-300 ease-in-out'
-							>">
-                <LogIn className='mr-2' size={18} />
-                Login</Link>
+							>"
+                >
+                  <LogIn className="mr-2" size={18} />
+                  Login
+                </Link>
               </>
             )}
           </nav>
