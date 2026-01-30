373fca578ef03e235b5ed0f6603bc9b2de186749
Aditya kumar <guptaditya81@gmail.com>
2026-01-30 23:53:48 +0530

    Fix undefined cart length error in Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,16 @@
 import { Lock, LogIn, LogOut, ShoppingCart, UserPlus } from "lucide-react";
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { userStore } from "../store/userStore";
+import { cartStore } from "../store/cartStore";
 
 const Navbar = () => {
   const { user, logout } = userStore(); // Replace with actual authentication logic
+  const {cart ,getCartItems} = cartStore();
+  useEffect(() => {
+    getCartItems();
+  }, [getCartItems]);
+  console.log("Cart items in Navbar:", cart);
   const isAdmin = user?.role === "admin";
   return (
     <header className="fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800">
@@ -31,14 +37,14 @@ const Navbar = () => {
                   size={20}
                 />
                 <span className="hidden sm:inline">Cart</span>
-                {/* {cart.length > 0 && ( */}
+                {cart?.length > 0 && (
                 <span
                   className="absolute -top-2 -left-2 bg-emerald-500 text-white rounded-full px-2 py-0.5 
 									text-xs group-hover:bg-emerald-400 transition duration-300 ease-in-out"
                 >
-                  {/* {cart.length} */}4
+                  {cart.length}
                 </span>
-                {/* )} */}
+              )}
               </Link>
             )}
 
