eb9d5fc6c5d7fe8ceabad902e70f9af8f306c164
Aditya kumar <guptaditya81@gmail.com>
2026-02-07 23:56:34 +0530

    fix: remove unique constraint on userId in coupon model to allow multiple coupons per user

--- backend/models/coupon.model.js
+++ backend/models/coupon.model.js
@@ -24,7 +24,6 @@ const couponSchema = new mongoose.Schema({
         type:mongoose.Schema.Types.ObjectId,
         ref:"User",
         required:true,
-        unique:true
     },
 },{timestamps:true})
 
