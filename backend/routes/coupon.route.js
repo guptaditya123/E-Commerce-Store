1682ab591eae1204b92d268259c1e6d688d16dc0
Aditya kumar <guptaditya81@gmail.com>
2026-02-10 23:40:18 +0530

    feat: update coupon routes configuration

--- backend/routes/coupon.route.js
+++ backend/routes/coupon.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { protectRoute } from '../middleware/auth.middleware.js';
-import { getCoupon, validateCoupon } from '../controllers/coupon.controller.js';
+import { createCoupon, getCoupon, validateCoupon } from '../controllers/coupon.controller.js';
 
 const couponRouter = express.Router();
 
 couponRouter.get('/',protectRoute,getCoupon);
 couponRouter.post('/',protectRoute,validateCoupon);
+couponRouter.post('/create',protectRoute,createCoupon);
 
 export default couponRouter;
