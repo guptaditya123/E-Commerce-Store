2fba1c04cb043495a8e74656037f8db3b97fa3f8
Aditya kumar <guptaditya81@gmail.com>
2026-02-14 23:38:47 +0530

    feat: add automatic token refresh with axios interceptors

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -6,5 +6,64 @@ const axiosInstance = axios.create({
 
 })
 
+let isRefreshing = false;
+let failedQueue = [];
+
+const processQueue = (error, token = null) => {
+    failedQueue.forEach(prom => {
+        if (error) {
+            prom.reject(error);
+        } else {
+            prom.resolve(token);
+        }
+    });
+    
+    failedQueue = [];
+};
+
+// Response interceptor to handle token refresh
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config;
+
+        // If error is 401 and we haven't tried to refresh yet
+        if (error.response?.status === 401 && !originalRequest._retry) {
+            if (isRefreshing) {
+                // If already refreshing, queue this request
+                return new Promise((resolve, reject) => {
+                    failedQueue.push({ resolve, reject });
+                }).then(() => {
+                    return axiosInstance(originalRequest);
+                }).catch(err => {
+                    return Promise.reject(err);
+                });
+            }
+
+            originalRequest._retry = true;
+            isRefreshing = true;
+
+            try {
+                // Try to refresh the token
+                await axiosInstance.post('/auth/refresh-token');
+                
+                isRefreshing = false;
+                processQueue(null, null);
+                
+                // Retry the original request
+                return axiosInstance(originalRequest);
+            } catch (refreshError) {
+                isRefreshing = false;
+                processQueue(refreshError, null);
+                
+                // If refresh fails, redirect to login
+                // Clear user state (handled by the store)
+                return Promise.reject(refreshError);
+            }
+        }
+
+        return Promise.reject(error);
+    }
+);
 
 export default axiosInstance;
